d662fadae88ae317fe6995dc50f324dc87da6aba
Adam Novak <adnosk@gmail.com>
2022-12-07 00:37:18 +0100

    Solve 02 exercise

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,6 +1,14 @@
-const repeatString = function() {
-
-};
+const repeatString = function(string, num) {
+    if (num < 0) {
+        return 'ERROR';
+    }
+    
+    let addString = "";
+    for (let i = 0; i < num; i++) {
+        addString += string;
+    }
+    return addString;
+}
 
 // Do not edit below this line
 module.exports = repeatString;
