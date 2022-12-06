bf7bfb3943e5824a2bbea974f1a9a7ec09f00639
Adam Novak <adnosk@gmail.com>
2022-12-07 00:49:05 +0100

    Solve 03 exercise

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,7 @@
-const reverseString = function() {
-
+const reverseString = function(word) {
+    const splitWord = word.split('');
+    splitWord.reverse();
+    return splitWord.join('');
 };
 
 // Do not edit below this line
