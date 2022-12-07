1c19f7f9a5af06fcc4940c14000e9e2bf55f310e
Adam Novak <adnosk@gmail.com>
2022-12-07 03:32:48 +0100

    Solve 04 exercise

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,10 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(array, ...values) {
+    const delArray = Array.from(values);
+    for (let i = 0; i < delArray.length; i++) {
+        array = array.filter(item => item !== delArray[i]);
+    }
+    
+    return array;
 };
 
 // Do not edit below this line
