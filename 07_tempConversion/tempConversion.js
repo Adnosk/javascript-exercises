249ddb8e84e2e25ab0d841c087d2a52eceea77cf
Adam Novak <adnosk@gmail.com>
2022-12-08 22:38:12 +0100

    Fix indentation

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,9 +1,9 @@
-const convertToCelsius = function(fahrenheit) {
+const convertToCelsius = function (fahrenheit) {
   return temp = Math.round(((fahrenheit - 32) * (5 / 9)) * 10) / 10;
 };
 
-const convertToFahrenheit = function(celsius) {
-  return temp = Math.round((celsius * (9/5) +32) * 10) /10;
+const convertToFahrenheit = function (celsius) {
+  return temp = Math.round((celsius * (9 / 5) + 32) * 10) / 10;
 };
 
 // Do not edit below this line
