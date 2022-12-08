1d782ca2451f5be61bcb26bb3372d606f91214b7
Adam Novak <adnosk@gmail.com>
2022-12-08 22:53:32 +0100

    Fix semicolon

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,10 +1,10 @@
 const leapYears = function (year) {
     if (year % 4 === 0 && !(year % 100 === 0)) {
-        return true
+        return true;
     } else if (year % 400 === 0) {
-        return true
+        return true;
     } else {
-        return false
+        return false;
     }
 };
 
