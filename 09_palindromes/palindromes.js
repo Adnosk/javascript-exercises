c340cf30f768b1b4b7f58ad8504ea30e8bf5a4c2
Adam Novak <adnosk@gmail.com>
2022-12-20 05:06:08 +0100

    Solve exercise

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,7 @@
-const palindromes = function () {
-
+const palindromes = function (word) {
+    const fixedWord = word.toLowerCase().replace(/[\W_]/g,'');
+    const flippedWord = fixedWord.split('').reverse().join('');
+    return fixedWord === flippedWord;
 };
 
 // Do not edit below this line
