8fab3a38a11b8503017a32630dac146020a5754a
Adam Novak <adnosk@gmail.com>
2022-12-20 04:46:23 +0100

    Solve exercise

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,30 @@
-const add = function() {
-	
+const add = function(a, b) {
+	return a + b;
 };
 
-const subtract = function() {
-	
+const subtract = function(a, b) {
+	return a - b;
 };
 
-const sum = function() {
-	
+const sum = function(array) {
+	return array.reduce((accumulator, number) => accumulator + number, 0);
 };
 
-const multiply = function() {
-
+const multiply = function(array) {
+  return array.length ? 
+  array.reduce((accumulator, number) => accumulator * number)
+  :0;
 };
 
-const power = function() {
-	
+const power = function(a,b) {
+	return a**b;
 };
 
-const factorial = function() {
-	
+const factorial = function(number) {
+	if (number === 0) {
+    return 1;
+  }
+  return number * factorial(number-1);
 };
 
 // Do not edit below this line
